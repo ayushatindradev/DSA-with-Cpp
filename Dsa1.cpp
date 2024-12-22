ba56ac90829584f07b0ad7af531dc2a6ed20242c
ayushatindradev <ayush.atindra.dev@gmail.com>
2024-12-22 23:11:56 +0530

    Add files via upload

---
+++ Dsa1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout<<"hello world";
+    // char ch;
+    // cin >> ch;
+    // cout <<"here: "<< ch <<endl;
+    // int marks;
+    // cout<< "Enter the marks here :"<< endl;
+    // // take input from user
+    // cin >> marks;
+
+    // cout << "printing the marks: " << marks <<endl;
+    // int age = 101;
+    // int car = 12;
+    // if(age >= 18 || car >=1){
+    //     cout << "Licence miljaiga " << endl;
+    // }
+    // cout << !age << endl;
+
+    // int a = 3;
+    // int b = 10;
+    // cout << (a == b) << endl;
+
+    // int a = 3;
+    // int b = 2;
+    // cout << a + b << endl;
+    // cout << a - b << endl;
+    // cout << a * b << endl;
+    // cout << b % a << endl; --doubt
+
+    // int num;
+    // // produces random values - garbage values
+    // cout << num;
+
+    // int num;
+    // num = 12;
+    // cout << num << endl;
+
+    // int ->4 byte;
+    // int num = 50;
+    // cout << num << endl;
+    // cout << sizeof(num) <<endl;
+
+    // // charecter -> 1 byte
+    // char ch = 'j';
+    // // cout << ch << endl;
+    // cout << sizeof(ch) << endl;
+
+    // float
+    // float point = 1.68;
+    // cout << point << endl;
+
+    // long
+    // long number = 23;
+    // cout << number << endl;
+    // cout << sizeof(number) <<endl;
+
+    // cout << "Ayush " << endl <<"Atindra "<<'\n';
+
+    // int num = 8;
+    // if(num % 2 == 0){
+    //     cout <<"Number is Even " << endl;
+    // }
+    // else{
+    // cout <<"Number is odd "<< endl;
+    // }
+    // int num = 56;
+    // if(num > 0){
+    //     cout<<"positive number " <<endl;
+
+    // }
+    // else if(num ==0){
+    //     cout << "Number is Zero "<<endl;
+    // }
+    // else{
+    //     cout<<"negative number " <<endl;
+    // }
+
+    
+
+    return 0;
+}
